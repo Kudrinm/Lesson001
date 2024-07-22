713c00c963c9c3c3a1b36293d83aa5d64f6d6206
Kudrinm <kudrinm@yandex.ru>
2024-07-22 14:06:39 +0300

    Add files via upload

---
+++ resolve031.py
@@ -0,0 +1,83 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('No floor')
+        else:
+            for n in range(1, new_floor + 1):
+                print(n)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return False
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1.number_of_floors)
+print(h1)
+print(h2)
+
+print(h1 == h2)  # eq
+
+h1 = h1 + 10  # add
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # iadd
+print(h1)
+
+h2 = 10 + h2  # radd
+print(h2)
+
+print(h1 > h2)  # gt
+print(h1 >= h2)  # ge
+print(h1 < h2)  # lt
+print(h1 <= h2)  # le
+print(h1 != h2)  # ne
